9321992b93e4ca2a610d7f712e7e6fe0e85d4a02
Prajil K <prajilk20017@gmail.com>
2025-07-06 17:31:20 +0530

    created user profile api

---
+++ src/app/api/app/user/route.ts
@@ -0,0 +1,22 @@
+import { error401, error500, success200 } from "@/lib/response";
+import { AuthenticatedAppRequest } from "@/lib/types/auth-request";
+import { withDbConnectAndAppAuth } from "@/lib/withDbConnectAndAppAuth";
+import User from "@/models/userModel";
+
+async function getHandler(req: AuthenticatedAppRequest) {
+    try {
+        if (!req.user) return error401("Unauthorized");
+
+        const user = await User.findOne(
+            { _id: req.user.id },
+            "name phone email"
+        );
+
+        return success200({ user });
+    } catch (error) {
+        if (error instanceof Error) return error500({ error: error.message });
+        else return error500({ error: "An unknown error occurred." });
+    }
+}
+
+export const GET = withDbConnectAndAppAuth(getHandler);
