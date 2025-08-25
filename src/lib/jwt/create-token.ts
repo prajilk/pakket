1311dd8d4dab81c93318f9a6bf3bad8c3d6c59cc
Prajil K <prajilk20017@gmail.com>
2025-08-25 11:05:16 +0530

    token 3m

--- src/lib/jwt/create-token.ts
+++ src/lib/jwt/create-token.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 function createToken(payload: string | Record<string, string | boolean>) {
     const token = jwt.sign(payload, process.env.NEXTAUTH_SECRET!, {
-        expiresIn: "7d",
+        expiresIn: "3m",
     });
 
     return token;
